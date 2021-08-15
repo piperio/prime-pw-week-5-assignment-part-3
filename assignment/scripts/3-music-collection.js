8d38f333b3a27803f7f1fd837ae9a00388ca32d5
Piper Van Ness <elysian.ojala@gmail.com>
2021-08-15 16:07:38 -0500

    working on part 3

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -2,21 +2,40 @@ console.log('***** Music Collection *****')
 
 let collection = [];
 
-function addToCollection(title, artist, yearPublished){
-  let album = {
-    title: title,
-    artist: artist,
-    yearPublished: yearPublished
-  };
+function addToCollection(){
+  let album = {title: title, artist: artist , yearPublished: yearPublished};
   collection.push(album);
   return album;
 }; //end addToCollection function
 
-album = ((title='Violator'), (artist='Depeche Mode'), (yearPublished='1990'))
+//album = ['Violator','Depeche Mode', 1990']
 //album = ('Siamese Dream', 'Smashing Pumpkins', '1993')
 //album = ('Nevermind', 'Nirvana', '1991')
 //album = ('Kid A', 'Radiohead', '2000')
 //album = ('Koto', 'Clozee', '2015')
 //album = ('Bury Me At Makeout Creek', 'Mitski', '2014')
 
-console.log(addToCollection(('Violator', 'Depeche Mode', '1990')));
+
+
+console.log(addToCollection(title='Violator', artist='Depeche Mode', yearPublished=1990));
+console.log(addToCollection(title='Siamese Dream', artist='Smashing Pumpkins', yearPublished=1993));
+console.log(addToCollection(title='Nevermind', artist='Nirvana', yearPublished=1991 ));
+console.log(addToCollection(title='Kid A', artist='Radiohead', yearPublished=2000));
+console.log(addToCollection(title='Koto', artist='Clozee', yearPublished=2015));
+console.log(addToCollection(title='Bury Me At Makeout Creek', artist='Mitski', yearPublished=2014));
+
+function showCollection(collection){
+  console.log(collection)
+};
+
+showCollection(collection);
+console.log(collection.length);
+
+showCollection(collection);
+
+function showCollection(collection){
+  for(let i = 0; i < collection.length; i++) {
+console.log(collection[i].title +' ' + 'by ' + collection[i].artist + ' '+ 'published in ' + '' + collection[i].yearPublished);
+  //console.log(collection.title + 'by' + collection.artist + 'published in' collection.yearPublished);
+}
+}
